8dc26e407adeb3cfc396d303b60e61a5886ad9dc
Shereena2000 <shereenajamezz@gmail.com>
2025-07-03 00:24:14 +0530

    feat: complete login page UI

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color redColor = Color(0xFFCC1F1F);
+const Color whiteColor = Colors.white;
+const Color blackColor = Color.fromARGB(255, 49, 49, 49);
+const Color greyColor = Colors.grey;
+const Color lightGreyColor = Color.fromARGB(255, 200, 199, 199);
+const Color screenColor = Color(0xFFF5F5F5);
